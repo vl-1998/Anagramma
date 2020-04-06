023a073542b1a5351ba86a530bc45d348ec3e717
sabinoMac13 <sabinoMac13@192.168.1.107>
2020-04-06 12:17:10 +0200

    Risoluzione anagrammi con ricorsione

--- src/main/java/it/polito/tdp/anagramma/model/Ricerca.java
+++ src/main/java/it/polito/tdp/anagramma/model/Ricerca.java
@@ -4,69 +4,73 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ricerca {
-	
-	private List<String> soluzione ;
+	private List <String> soluzione;
 
 	/**
-	 * Genera tutti gli anagrammi della parola specificata in ingresso.
-	 * @param parola parola da anagrammare
+	 * Genera tutti gli anagrammi della parola specificata in ingresso
+	 * @param string parola da anagrammare
 	 * @return elenco di tutti gli anagrammi della parola data
 	 */
 	public List<String> anagrammi(String parola) {
-		this.soluzione = new ArrayList<>() ;
+		//caso iniziale
+		this.soluzione= new ArrayList<String>();
+		//devo preparare tutte le variabili di cui ha bisogno la funzione ricorsiva
+		parola=parola.toUpperCase();
 		
-		parola=parola.toUpperCase() ;
-		
-		List<Character> disponibili = new ArrayList<>() ;
-		for(int i=0; i<parola.length(); i++) {
-			disponibili.add(parola.charAt(i)) ;
+		List <Character> disponibili = new ArrayList <>();
+		for (int i=0; i<parola.length(); i++) {
+			disponibili.add(parola.charAt(i));//estraggo il carattere in posizione i-esima nella stringa e lo aggiungo ai caratteri disponibili
 		}
 		
-		// avvia la ricorsione
-		cerca("", 0, disponibili) ; 
+		//avvio la ricorsione
+		cerca("",0,disponibili); 
 		
-		return this.soluzione ;
+		return this.soluzione;
 	}
 	
+	//funzione ricorsiva che sfruttiamo per costruirla
 	/**
-	 * Procedura ricorsiva per il calcolo dell'anagramma.
+	 * Procedura ricorsiva oer il calcolo dell'anagramma 
 	 * 
-	 * @param parziale parte iniziale dell'anagramma costruito finora
-	 * @param livello livello della ricorsione, sempre uguale a parziale.length()
-	 * @param disponibili insieme delle lettere non ancora utilizzate
+	 * @param parziale parte iniziale del'anagramma costruito fin'ora
+	 * @param livello livello della ricorsione, sempre uguale a parziale.lenght()
+	 * @param disponibili insieme delle lettere non ancora utilizzate (rappresentato come una lista perche l'insieme puo contenere dei duplicati)
 	 */
-	private void cerca( String parziale, int livello, List<Character> disponibili) {
-		if(disponibili.size()==0) { // livello==parola.length()
-			// caso terminale
+	private void cerca (String parziale, int livello, List<Character> disponibili) {
+		if (disponibili.size()==0) { //livello==parola.lenght()
+			//caso terminale
 			
-			// if(parziale è nel dizionario)
-			// if( parziale non è presente nella soluzione )
-			this.soluzione.add(parziale) ;
+			//if (parziale è nel dizionario) la aggiungo. Ho comunque calcolato tutto, ma non stampo tutto
+			//if (parziale non è presente nella soluzione) adesso lo aggiungo
+			this.soluzione.add(parziale);
+			
 		}
 		
-		// caso normale
-		// provare ad aggiungere a 'parziale' tutti i caratteri presenti tra
-		// i 'disponibili'
-		for(Character ch: disponibili) {
-			String tentativo = parziale + ch ;
+		//caso normale
+		//provare ad aggiungere alla soluzione parziale tutti i caratteri presenti tra i disponibili
+		for (Character ch: disponibili) {
+			String tentativo = parziale + ch; //non posso andare a modificare la stringa parziale, quindi ne devo creare una temporanea. 
+												// non ho più bisogno del backtracking
 			
-//			if(nel dizionario esistono delle parole che iniziano con 'tentativo'?)
+			//if (nel dizionario esistono delle parole che iniziano con 'tentativo'?)
+			//se si continuo con la ricorsione, altrimenti mi fermo prima
 			
-			List<Character> rimanenti = new ArrayList<>(disponibili) ;
-			rimanenti.remove(ch) ;
-			
-			cerca( tentativo, livello+1, rimanenti) ;
+			List <Character> rimanenti = new ArrayList <>(disponibili); //ne devo creare un'altra perche lo sto iterano in questo momento
+			rimanenti.remove(ch); //però questa cosa rallenta molto il programma
+			cerca(tentativo, livello+1, rimanenti);
 		}
 	}
-
+	
 }
 
-/*
-Dato di partenza: parola da anagrammare, di lunghezza N
-Soluzione parziale: una parte dell'anagramma già costruito (i primi caratteri).
-Livello: numero di lettere di cui è composta la soluzione parziale.
-Soluzione finale: soluzione di lunghezza N -> caso terminale
-Caso terminale: salvare la soluzione trovate
-Generazione delle nuove soluzioni: provare a aggiungere una lettera, scegliendola
-tra quelle che non sono ancora state utilizzate (nella soluzione parziale).
-*/
+/* 
+ * Dato di partenza: parola da angrammare, di lunghezza N
+ * SOLUZIONE PARZIALE:parte dell'anagramma gia costruito (i primi caratteri della parola)
+ * Livello: numero di lettere di cui è composta la soluzione parziale
+ * Soluzione finale: soluzione di lunghezza N (qui capisco che sono in un caso terminale)
+ * Caso terminale: salvare le soluzioni trovate
+ * 
+ * Generazione delle nuove soluzioni: provare ad aggiungere una lettera scegliendola tra quelle che non sono ancora state utilizzate
+ * nella soluzione parziale
+ */
+ 
--- src/main/java/it/polito/tdp/anagramma/model/TestRicerca.java
+++ src/main/java/it/polito/tdp/anagramma/model/TestRicerca.java
@@ -10,12 +10,12 @@ public class TestRicerca {
 		List<String> ana_dog = ric.anagrammi("dog") ;
 		System.out.println(ana_dog) ;
 		
-		List<String> ana_vac = ric.anagrammi("vacanze") ;
+		List<String> ana_vac = ric.anagrammi("vacaze") ;
 		System.out.println(ana_vac) ;
-
-		List<String> ana_prg = ric.anagrammi("programmazione") ;
-		System.out.println(ana_prg) ;
-
+		
+		/*List<String> ana_prg = ric.anagrammi("programmazione") ;
+		System.out.println(ana_prg) ;*/
+		
 	}
 
 }
